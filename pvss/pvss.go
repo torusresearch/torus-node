378a11c1d661af1ca9d0f13adc17982313e3464f
Zhen Yu Yong <zhen@Zhens-MacBook-Pro.local>
2018-10-24 15:04:36 -0700

    packaged pkcs7 differently

--- pvss/pvss.go
+++ pvss/pvss.go
@@ -1,7 +1,6 @@
 package pvss
 
 import (
-	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
@@ -10,6 +9,7 @@ import (
 	"io"
 	"math/big"
 
+	"../pkcs7"
 	"github.com/decred/dcrd/dcrec/secp256k1"
 	"github.com/ethereum/go-ethereum/crypto/sha3"
 )
@@ -215,7 +215,7 @@ func generateRandomPolynomial(secret big.Int, threshold int) *PrimaryPolynomial 
 func AESencrypt(key []byte, plainText []byte) (c *[]byte, err error) {
 	//Following PKCS#7 described in RFC 5652 for padding key
 
-	key, err = pkcs7Pad(key, 32)
+	key, err = pkcs7.pkcs7Pad(key, 32)
 	if err != nil {
 		return nil, err
 	}
@@ -242,7 +242,7 @@ func AESencrypt(key []byte, plainText []byte) (c *[]byte, err error) {
 func AESdecrypt(key []byte, cipherText []byte) (message *[]byte, err error) {
 	//Following PKCS#7 described in RFC 5652 for padding key
 
-	key, err = pkcs7Pad(key, 32)
+	key, err = pkcs7.pkcs7Pad(key, 32)
 	if err != nil {
 		return nil, err
 	}
@@ -402,60 +402,4 @@ func verifyProof(proof DLEQProof, nodePubKey Point) bool {
 		return false
 	}
 	return true
-}
-
-// PKCS7 padding.
-// PKCS7 errors.
-var (
-	// ErrInvalidBlockSize indicates hash blocksize <= 0.
-	ErrInvalidBlockSize = errors.New("invalid blocksize")
-
-	// ErrInvalidPKCS7Data indicates bad input to PKCS7 pad or unpad.
-	ErrInvalidPKCS7Data = errors.New("invalid PKCS7 data (empty or not padded)")
-
-	// ErrInvalidPKCS7Padding indicates PKCS7 unpad fails to bad input.
-	ErrInvalidPKCS7Padding = errors.New("invalid padding on input")
-)
-
-// pkcs7Pad right-pads the given byte slice with 1 to n bytes, where
-// n is the block size. The size of the result is x times n, where x
-// is at least 1.
-func pkcs7Pad(b []byte, blocksize int) ([]byte, error) {
-	if blocksize <= 0 {
-		return nil, ErrInvalidBlockSize
-	}
-	if b == nil || len(b) == 0 {
-		return nil, ErrInvalidPKCS7Data
-	}
-	n := blocksize - (len(b) - 1%blocksize) - 1
-	pb := make([]byte, len(b)+n)
-	copy(pb, b)
-	copy(pb[len(b):], bytes.Repeat([]byte{byte(n)}, n))
-	return pb, nil
-}
-
-// pkcs7Unpad validates and unpads data from the given bytes slice.
-// The returned value will be 1 to n bytes smaller depending on the
-// amount of padding, where n is the block size.
-func pkcs7Unpad(b []byte, blocksize int) ([]byte, error) {
-	if blocksize <= 0 {
-		return nil, ErrInvalidBlockSize
-	}
-	if b == nil || len(b) == 0 {
-		return nil, ErrInvalidPKCS7Data
-	}
-	if len(b)%blocksize != 0 {
-		return nil, ErrInvalidPKCS7Padding
-	}
-	c := b[len(b)-1]
-	n := int(c)
-	if n == 0 || n > len(b) {
-		return nil, ErrInvalidPKCS7Padding
-	}
-	for i := 0; i < n; i++ {
-		if b[len(b)-n+i] != c {
-			return nil, ErrInvalidPKCS7Padding
-		}
-	}
-	return b[:len(b)-n], nil
 }

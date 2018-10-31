f55a22e4747ada5dd57ce0d7269be0d885b0d467
tetratorus <tetratorus@gmail.com>
2018-10-31 14:21:45 +0100

    Add register flag

--- cmd/DKGNode/main.go
+++ cmd/DKGNode/main.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	/* Parse the provided parameters on command line */
-	configPath := flag.String("configPath", "./cmd/DKGNode/config.json", "provide path to config file, defaults ./cmd/DKGNode/config.json")
+	register := flag.Bool("register", true, "defaults true")
+	configPath := flag.String("configPath", "./config.json", "provide path to config file, defaults ./cmd/DKGNode/config.json")
 	flag.Parse()
-	dkgnode.New(*configPath)
+	dkgnode.New(*configPath, *register)
 }
--- dkgnode/dkgnode.go
+++ dkgnode/dkgnode.go
@@ -13,7 +13,7 @@ type Suite struct {
 }
 
 /* The entry point for our System */
-func New(configPath string) {
+func New(configPath string, register bool) {
 
 	//Main suite of functions used in node
 	suite := Suite{}
@@ -31,9 +31,12 @@ func New(configPath string) {
 	// 	fmt.Println(err)
 	// }
 	fmt.Println("Node IP Address: " + suite.Config.HostName + ":" + string(suite.Config.MyPort))
-	_, err = suite.EthSuite.registerNode(suite.Config.HostName + ":" + string(suite.Config.MyPort))
-	if err != nil {
-		log.Fatal(err)
+	if register {
+		fmt.Println("Registering node...")
+		_, err = suite.EthSuite.registerNode(suite.Config.HostName + ":" + string(suite.Config.MyPort))
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	// test := make([]string, 1)

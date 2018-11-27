25be96fa71bcda371014b61e072a9be370f2face
tetratorus <tetratorus@gmail.com>
2018-11-27 14:28:48 -0800

    Update list of ports to kill for killNodes

--- killNodes.sh
+++ killNodes.sh
@@ -7,3 +7,8 @@ lsof -n -i:8002 | grep LISTEN | awk '{ print $2 }' | xargs kill
 lsof -n -i:8003 | grep LISTEN | awk '{ print $2 }' | xargs kill
 lsof -n -i:8004 | grep LISTEN | awk '{ print $2 }' | xargs kill
 lsof -n -i:8005 | grep LISTEN | awk '{ print $2 }' | xargs kill
+lsof -n -i:8010 | grep LISTEN | awk '{ print $2 }' | xargs kill
+lsof -n -i:8011 | grep LISTEN | awk '{ print $2 }' | xargs kill
+lsof -n -i:8012 | grep LISTEN | awk '{ print $2 }' | xargs kill
+lsof -n -i:8013 | grep LISTEN | awk '{ print $2 }' | xargs kill
+lsof -n -i:8014 | grep LISTEN | awk '{ print $2 }' | xargs kill

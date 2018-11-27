1d3dcc4efeec2169ff86fd8bd7e2e716a77b4f2b
YZhenY <fuz3zy@gmail.com>
2018-11-27 14:38:43 -0800

    edited set root to use bfturi

--- cmd/dkgnode/dkgnode.go
+++ cmd/dkgnode/dkgnode.go
@@ -13,11 +13,12 @@ func main() {
 	register := flag.Bool("register", true, "defaults to true")
 	production := flag.Bool("production", false, "defaults to false")
 	configPath := flag.String("configPath", "", "provide path to config file")
+	buildPath := flag.String("buildPath", "./.build", "provide path to build file")
 
 	flag.Parse()
 	if *configPath == "" {
 		log.Fatal(errors.New("No configuration path provided, aborting"))
 	} else {
-		dkgnode.New(*configPath, *register, *production)
+		dkgnode.New(*configPath, *register, *production, *buildPath)
 	}
 }

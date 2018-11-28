fddfa5fd252739f187c6e54085112b4ec468a87d
YZhenY <fuz3zy@gmail.com>
2018-11-27 16:31:04 -0800

    implemented function to convert X,Y to tm secp256k1 formats

--- dkgnode/dkgnode.go
+++ dkgnode/dkgnode.go
@@ -1,13 +1,16 @@
 package dkgnode
 
-/* All useful imports */
+//TODO: export all "tm" imports to common folder
 import (
 	"fmt"
 	"log"
+	"math/big"
 	"os"
 	"time"
 
+	tmbtcec "github.com/tendermint/btcd/btcec"
 	tmconfig "github.com/tendermint/tendermint/config"
+	tmsecp "github.com/tendermint/tendermint/crypto/secp256k1"
 	tmlog "github.com/tendermint/tendermint/libs/log"
 	tmnode "github.com/tendermint/tendermint/node"
 	"github.com/tendermint/tendermint/p2p"
@@ -57,7 +60,32 @@ func New(configPath string, register bool, production bool, buildPath string) {
 	// defaultTmConfig.PrivValidator = "config/"
 	defaultTmConfig.ProxyApp = suite.Config.ABCIServer
 
-	pv := privval.GenFilePVSecp(defaultTmConfig.PrivValidatorFile())
+	//converts own pv to tendermint key TODO: Double check verification
+	var testpv tmsecp.PrivKeySecp256k1
+	for i := range suite.EthSuite.NodePrivateKey.D.Bytes() {
+		testpv[i] = suite.EthSuite.NodePrivateKey.D.Bytes()[i]
+	}
+	//SEEMS RIGHT (there are some bytes earlier but they use btcecc)
+	//From their docs
+	// PubKeySecp256k1Size is comprised of 32 bytes for one field element
+	// (the x-coordinate), plus one byte for the parity of the y-coordinate.
+	fmt.Println("ETH PUB KEY: ", suite.EthSuite.NodePublicKey.X.Bytes())
+	fmt.Println("TM PUB KEY: ", testpv.PubKey().Bytes())
+
+	//convert pubkey X and Y to tmpubkey
+	pubkeyBytes := RawPointToTMPubKey(suite.EthSuite.NodePublicKey.X, suite.EthSuite.NodePublicKey.Y)
+
+	fmt.Println("DOEST IT EQUAL", pubkeyBytes.Equals(testpv.PubKey()))
+
+	// pv := &privval.FilePV{
+	// 	Address:  testpv.PubKey().Address(),
+	// 	PubKey:   testpv.PubKey(),
+	// 	PrivKey:  testpv,
+	// 	LastStep: int8(0),
+	// 	// filePath: defaultTmConfig.PrivValidatorFile(),
+	// }
+
+	// pv := privval.GenFilePVSecp(defaultTmConfig.PrivValidatorFile())
 	nodeKey, err := p2p.LoadOrGenNodeKey(defaultTmConfig.NodeKeyFile())
 	if err != nil {
 		fmt.Println(err)
@@ -69,8 +97,8 @@ func New(configPath string, register bool, production bool, buildPath string) {
 		ConsensusParams: tmtypes.DefaultConsensusParams(),
 	}
 	genDoc.Validators = []tmtypes.GenesisValidator{{
-		Address: pv.GetPubKey().Address(),
-		PubKey:  pv.GetPubKey(),
+		Address: pubkeyBytes.Address(),
+		PubKey:  pubkeyBytes,
 		Power:   10,
 	}}
 
@@ -126,6 +154,7 @@ func New(configPath string, register bool, production bool, buildPath string) {
 	logger.Info("Started tendermint node", "nodeInfo", n.Switch().NodeInfo())
 
 	go keyGenerationPhase(&suite)
+
 	setUpServer(&suite, string(suite.Config.MyPort))
 }
 
@@ -133,4 +162,15 @@ func ProvideGenDoc(doc *tmtypes.GenesisDoc) tmnode.GenesisDocProvider {
 	return func() (*tmtypes.GenesisDoc, error) {
 		return doc, nil
 	}
+}
+
+func RawPointToTMPubKey(X, Y *big.Int) tmsecp.PubKeySecp256k1 {
+	//convert pubkey X and Y to tmpubkey
+	var pubkeyBytes tmsecp.PubKeySecp256k1
+	pubkeyObject := tmbtcec.PublicKey{
+		X: X,
+		Y: Y,
+	}
+	copy(pubkeyBytes[:], pubkeyObject.SerializeCompressed())
+	return pubkeyBytes
 }

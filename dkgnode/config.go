6ff8e7e14065565254f4dfabc8d1750d2ff9c1d2
YZhenY <fuz3zy@gmail.com>
2018-12-03 22:53:23 -0800

    added default commit file

--- dkgnode/config.go
+++ dkgnode/config.go
@@ -31,3 +31,19 @@ func loadConfig(suite *Suite, path string) {
 
 	suite.Config = &conf
 }
+
+func defaultConfigSettings() Config {
+	return Config{
+		MyPort:           "8000",
+		EthConnection:    "http://localhost:8545",
+		EthPrivateKey:    "29909a750dc6abc3e3c83de9c6da9d6faf9fde4eebb61fa21221415557de5a0b",
+		BftURI:           "tcp://0.0.0.0:26657",
+		NodeListAddress:  "0xd44f7724b0a0800e41283e97be5ec9e875f59811",
+		HostName:         "dkg1.tetrator.us",
+		NumberOfNodes:    5,
+		Threshold:        3,
+		ABCIServer:       "tcp://0.0.0.0:8010",
+		P2PListenAddress: "tcp://0.0.0.0:26656",
+		KeysPerEpoch:     10,
+	}
+}

fb5e1303c77748d6325201a45bfaa6c32cedd4c7
oskar <oskar@paolini.pl>
2019-03-21 22:12:18 +0100

    init TorusDB

--- dkgnode/db.go
+++ dkgnode/db.go
@@ -6,24 +6,48 @@ import (
 	"github.com/patrickmn/go-cache"
 )
 
+// TorusDB represents a set of methods necessary for the operation of torus node
+// NOTE: work in progress
+type TorusDB interface {
+	StoreLog(address string, log *ShareLog) error
+	GetLog(address string) (*ShareLog, error)
+	// AppendLog ?
+
+	StoreAddressMapping(address string) error
+	GetAddressMapping(address string) error
+	// Modify mapping by index??
+
+	StoreShareIndexMapping() error
+	GetShareIndexMapping() error
+
+	StoreSecretMapping() error
+	GetSecretMapping() error
+}
+
 type CacheSuite struct {
-	CacheInstance      *cache.Cache
-	OAuthCacheInstance *cache.Cache
+	CacheInstance *cache.Cache
+	// TODO(TEAM) - Not used, shouldn't it be removed?
+	// OAuthCacheInstance *cache.Cache
 }
 
 func SetUpCache(suite *Suite) {
 	// Create a cache with a default expiration time of no expiration time, and which
 	// purges expired items every 10 minutes
 	secretCache := cache.New(cache.NoExpiration, 10*time.Minute)
-	oauthCache := cache.New(60*time.Second, 1*time.Minute)
+
+	// TODO: UNUSED!
+	// oauthCache := cache.New(60*time.Second, 1*time.Minute)
 
 	suite.CacheSuite = &CacheSuite{
 		secretCache,
-		oauthCache,
+		// TODO: UNUSED!
+		// oauthCache,
 	}
 
-	secretAssignment := make(map[string]SecretAssignment)
-	suite.CacheSuite.CacheInstance.Set("Secret_ASSIGNMENT", secretAssignment, -1)
-	suite.CacheSuite.CacheInstance.Set("LAST_ASSIGNED", 0, -1)
+	// TODO(TEAM) - these are literally unused anywhere, should they still be here??
+	// secretAssignment := make(map[string]SecretAssignment)
+	//
+	// suite.CacheSuite.CacheInstance.Set("Secret_ASSIGNMENT", secretAssignment, -1)
+	// suite.CacheSuite.CacheInstance.Set("LAST_ASSIGNED", 0, -1)
 
 }

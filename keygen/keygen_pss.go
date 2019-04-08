a12243c85f5fd6cc8f1f6091cdf964f46bd9ef06
tetratorus <tetratorus@gmail.com>
2019-04-08 17:04:25 +0800

    Add types, implement send

--- keygen/keygen_pss.go
+++ keygen/keygen_pss.go
@@ -1,44 +1,58 @@
 package keygen
 
 import (
+	"errors"
 	"math/big"
 	"strconv"
 	"strings"
 	"sync"
+
+	"github.com/torusresearch/torus-public/pvss"
 
 	"github.com/intel-go/fastjson"
 
 	"github.com/torusresearch/torus-public/common"
 )
 
+type SharingID string
 type Sharing struct {
-	Nodes   []common.Node
-	Epoch   int
-	PSSID   PSSID
-	I       int
-	Si      big.Int
-	Siprime big.Int
-	C       []common.Point
+	sync.Mutex
+	SharingID SharingID
+	Nodes     []common.Node
+	Epoch     int
+	I         int
+	Si        big.Int
+	Siprime   big.Int
+	C         []common.Point
 }
 
 type PSS struct {
 	sync.Mutex
-	ID       big.Int
+	PSSID    PSSID
+	F        [][]big.Int
+	Fprime   [][]big.Int
 	Messages []PSSMessage
-	C        [][]common.Point
-	EC       int
-	RC       int
-	AC       []common.Point
-	ACprime  []common.Point
-	BC       []common.Point
-	BCprime  []common.Point
-	State    string
+	CStore   map[PSSCID]PSSC
+	State    PSSState
+}
+
+type PSSCID string
+
+type PSSC struct {
+	PSSCID  PSSCID
+	C       [][]common.Point
+	EC      int
+	RC      int
+	AC      []common.Point
+	ACprime []common.Point
+	BC      []common.Point
+	BCprime []common.Point
 }
 
 type PSSMessage struct {
-	PSSID PSSID  `json:"pssid"`
-	Type  string `json:"type"`
-	Data  string `json:"data"`
+	PSSID  PSSID  `json:"pssid"`
+	Method string `json:"type"`
+	Data   []byte `json:"data"`
 }
 
 func (pssMessage *PSSMessage) JSON() *fastjson.RawMessage {
@@ -55,44 +69,37 @@ func (pssMessage *PSSMessage) JSON() *fastjson.RawMessage {
 type PSSID string
 
 type PSSIDDetails struct {
-	ID   big.Int
-	Step string
-	From int
-	To   int
+	SharingID SharingID
+	Index     int
 }
 
-func (pssIDDetails *PSSIDDetails) ToString() string {
-	return pssIDDetails.ID.Text(16) + "|" + pssIDDetails.Step + "|" + strconv.Itoa(pssIDDetails.From) + "|" + strconv.Itoa(pssIDDetails.To)
+func (pssIDDetails *PSSIDDetails) ToPSSID() PSSID {
+	return PSSID(string(pssIDDetails.SharingID) + "|" + strconv.Itoa(pssIDDetails.Index))
 }
-func (pssIDDetails *PSSIDDetails) FromString(s string) {
+func (pssIDDetails *PSSIDDetails) FromPSSID(pssID PSSID) error {
+	s := string(pssID)
 	substrings := strings.Split(s, "|")
-	if len(substrings) != 4 {
-		return
+	if len(substrings) != 2 {
+		return errors.New("Error parsing PSSIDDetails, too few fields")
 	}
-	id, ok := new(big.Int).SetString(substrings[0], 16)
-	if !ok {
-		return
-	}
-	pssIDDetails.ID = *id
-	pssIDDetails.Step = substrings[1]
-	from, err := strconv.Atoi(substrings[2])
+	pssIDDetails.SharingID = SharingID(substrings[0])
+	index, err := strconv.Atoi(substrings[1])
 	if err != nil {
-		return
+		return err
 	}
-	pssIDDetails.From = from
-	to, err := strconv.Atoi(substrings[3])
-	if err != nil {
-		return
-	}
-	pssIDDetails.To = to
+	pssIDDetails.Index = index
+	return nil
 }
+
+type NodeDetailsID string
 
 type NodeDetails common.Node
 
-func (n *NodeDetails) ToString() string {
-	return strconv.Itoa(n.Index) + "|" + n.PubKey.X.Text(16) + "|" + n.PubKey.Y.Text(16)
+func (n *NodeDetails) ToNodeDetailsID() NodeDetailsID {
+	return NodeDetailsID(strconv.Itoa(n.Index) + "|" + n.PubKey.X.Text(16) + "|" + n.PubKey.Y.Text(16))
 }
-func (n *NodeDetails) FromString(s string) {
+func (n *NodeDetails) FromNodeDetailsID(nodeDetailsID NodeDetailsID) {
+	s := string(nodeDetailsID)
 	substrings := strings.Split(s, "|")
 	if len(substrings) != 3 {
 		return
@@ -123,11 +130,12 @@ var LocalNodeDirectory map[string]*LocalTransport
 
 type LocalTransport struct {
 	PSSNode       PSSNode
-	NodeDirectory *map[string]*LocalTransport
+	NodeDirectory *map[NodeDetailsID]*LocalTransport
+	// TODO: implement middleware feature
 }
 
 func (l *LocalTransport) Send(nodeDetails NodeDetails, pssMessage PSSMessage) error {
-	return (*l.NodeDirectory)[nodeDetails.ToString()].Receive(l.PSSNode.NodeDetails, pssMessage)
+	return (*l.NodeDirectory)[nodeDetails.ToNodeDetailsID()].Receive(l.PSSNode.NodeDetails, pssMessage)
 }
 
 func (l *LocalTransport) Receive(senderDetails NodeDetails, pssMessage PSSMessage) error {
@@ -146,12 +154,123 @@ type PSSNode struct {
 	OldNodes    NodeNetwork
 	NewNodes    NodeNetwork
 	NodeIndex   big.Int
-	ShareStore  map[string]Sharing
+	ShareStore  map[SharingID]*Sharing
 	Transport   PSSTransport
-	Resharings  []PSS
+	PSSStore    map[PSSID]*PSS
 }
 
 func (pssNode *PSSNode) ProcessMessage(nodeDetails NodeDetails, pssMessage PSSMessage) error {
+	if _, found := pssNode.PSSStore[pssMessage.PSSID]; !found {
+		pssNode.PSSStore[pssMessage.PSSID] = &PSS{
+			PSSID: pssMessage.PSSID,
+			State: PSSState{
+				PSSTypes.Phases.Initial,
+				PSSTypes.Dealer.NotDealer,
+				PSSTypes.Player.NotPlayer,
+				PSSTypes.ReceivedSend.False,
+			},
+		}
+	}
+	pss := pssNode.PSSStore[pssMessage.PSSID]
+	pss.Mutex.Lock()
+	defer pss.Mutex.Unlock()
+
+	pss.Messages = append(pss.Messages, pssMessage)
+
+	// handle different messages here
+	if pssMessage.Method == "share" {
+		// state checks
+		if pss.State.Phase == PSSTypes.Phases.Ended {
+			return errors.New("PSS has ended, ignored message " + string(*pssMessage.JSON()) + " from " + string(nodeDetails.ToNodeDetailsID()) + " ")
+		}
+		if pss.State.Dealer != PSSTypes.Dealer.IsDealer {
+			return errors.New("PSS could not be started since the node is not a dealer")
+		}
+
+		// logic
+		var pssMsgShare PSSMsgShare
+		err := pssMsgShare.FromBytes(pssMessage.Data)
+		if err != nil {
+			return errors.New("Could not unmarshal data for pssMsgShare")
+		}
+		sharing, found := pssNode.ShareStore[pssMsgShare.SharingID]
+		if !found {
+			return errors.New("Could not find sharing for sharingID " + string(pssMsgShare.SharingID))
+		}
+		pss.F = pvss.GenerateRandomBivariatePolynomial(sharing.Si, pssNode.NewNodes.K)
+		pss.Fprime = pvss.GenerateRandomBivariatePolynomial(sharing.Siprime, pssNode.NewNodes.K)
+		// pss.C = pvss.GetCommitmentMatrix(pss.F, pss.Fprime)
+
+		for _, newNode := range pssNode.NewNodes.Nodes {
+			nodeDetails := NodeDetails(newNode)
+			pssID := (&PSSIDDetails{
+				SharingID: sharing.SharingID,
+				Index:     sharing.I,
+			}).ToPSSID()
+			pssMsgSend := &PSSMsgSend{
+				PSSID: pssID,
+				// C:      pss.C,
+				A:      pvss.EvaluateBivarPolyAtX(pss.F, *big.NewInt(int64(nodeDetails.Index))).Coeff,
+				Aprime: pvss.EvaluateBivarPolyAtX(pss.Fprime, *big.NewInt(int64(nodeDetails.Index))).Coeff,
+				B:      pvss.EvaluateBivarPolyAtY(pss.F, *big.NewInt(int64(nodeDetails.Index))).Coeff,
+				Bprime: pvss.EvaluateBivarPolyAtY(pss.Fprime, *big.NewInt(int64(nodeDetails.Index))).Coeff,
+			}
+			nextPSSMessage := PSSMessage{
+				PSSID:  pssID,
+				Method: "send",
+				Data:   pssMsgSend.ToBytes(),
+			}
+			go func() {
+				pssNode.Transport.Send(nodeDetails, nextPSSMessage)
+			}()
+		}
+	} else if pssMessage.Method == "send" {
+		// state checks
+		if pss.State.Phase == PSSTypes.Phases.Ended {
+			return errors.New("PSS has ended, ignored message " + string(*pssMessage.JSON()) + " from " + string(nodeDetails.ToNodeDetailsID()) + " ")
+		}
+		if pss.State.Player != PSSTypes.Player.IsPlayer {
+			return errors.New("Could not receive send message because node is not a player")
+		}
+		if pss.State.ReceivedSend == PSSTypes.ReceivedSend.True {
+			return errors.New("Already received a send message for PSSID " + string(pss.PSSID))
+		}
+
+		// logic
+		var pssMsgSend PSSMsgSend
+		err := pssMsgSend.FromBytes(pssMessage.Data)
+		if err != nil {
+			return err
+		}
+		// pvss.AVSSVerifyPoly(pssMsgSend.C)
+
+	} else if pssMessage.Method == "echo" {
+		// state checks
+		if pss.State.Phase == PSSTypes.Phases.Ended {
+			return errors.New("PSS has ended, ignored message " + string(*pssMessage.JSON()) + " from " + string(nodeDetails.ToNodeDetailsID()) + " ")
+		}
+
+		// logic
+
+	} else if pssMessage.Method == "ready" {
+		// state checks
+		if pss.State.Phase == PSSTypes.Phases.Ended {
+			return errors.New("PSS has ended, ignored message " + string(*pssMessage.JSON()) + " from " + string(nodeDetails.ToNodeDetailsID()) + " ")
+		}
+
+		// logic
+
+	} else if pssMessage.Method == "shared" {
+		// state checks
+		if pss.State.Phase == PSSTypes.Phases.Ended {
+			return errors.New("PSS has ended, ignored message " + string(*pssMessage.JSON()) + " from " + string(nodeDetails.ToNodeDetailsID()) + " ")
+		}
+
+		// logic
+
+	} else {
+		return errors.New("PssMessage method not found")
+	}
 	return nil
 }
 
@@ -164,7 +283,7 @@ func NewPSSNode(
 	newNodesT int,
 	newNodesK int,
 	nodeIndex big.Int,
-	shareStore map[string]Sharing,
+	shareStore map[SharingID]*Sharing,
 	transport PSSTransport,
 ) *PSSNode {
 	return &PSSNode{
---
+++ keygen/types.go
@@ -0,0 +1,223 @@
+package keygen
+
+import (
+	"math/big"
+
+	"github.com/intel-go/fastjson"
+	"github.com/torusresearch/torus-public/common"
+	"github.com/torusresearch/torus-public/logging"
+	fjson "github.com/valyala/fastjson"
+)
+
+// Dont try to cast, use the exported structs to get the types you need
+type pssPhase string
+type pssDealer bool
+type pssPlayer bool
+type pssReceivedSend bool
+
+type pssPhases struct {
+	Initial   pssPhase
+	Started   pssPhase
+	Proposing pssPhase
+	Ended     pssPhase
+}
+
+type pssDealers struct {
+	IsDealer  pssDealer
+	NotDealer pssDealer
+}
+
+type pssPlayers struct {
+	IsPlayer  pssPlayer
+	NotPlayer pssPlayer
+}
+
+type pssReceivedSends struct {
+	True  pssReceivedSend
+	False pssReceivedSend
+}
+
+type PSSState struct {
+	Phase        pssPhase
+	Dealer       pssDealer
+	Player       pssPlayer
+	ReceivedSend pssReceivedSend
+}
+
+var PSSTypes = struct {
+	Phases       pssPhases
+	Dealer       pssDealers
+	Player       pssPlayers
+	ReceivedSend pssReceivedSends
+}{
+	pssPhases{
+		Initial:   pssPhase("initial"),
+		Started:   pssPhase("started"),
+		Proposing: pssPhase("proposing"),
+		Ended:     pssPhase("ended"),
+	},
+	pssDealers{
+		IsDealer:  pssDealer(true),
+		NotDealer: pssDealer(false),
+	},
+	pssPlayers{
+		IsPlayer:  pssPlayer(true),
+		NotPlayer: pssPlayer(false),
+	},
+	pssReceivedSends{
+		True:  pssReceivedSend(true),
+		False: pssReceivedSend(false),
+	},
+}
+
+type PSSMsgShare struct {
+	SharingID SharingID
+}
+
+func (pssMsgShare *PSSMsgShare) FromBytes(byt []byte) error {
+	var p fjson.Parser
+	val, err := p.Parse(string(byt))
+	if err != nil {
+		return err
+	}
+	pssMsgShare.SharingID = SharingID(val.GetStringBytes("sharingid"))
+	return nil
+}
+
+func (pssMsgShare *PSSMsgShare) ToBytes() []byte {
+	byt, err := fastjson.Marshal(pssMsgShare)
+	if err != nil {
+		logging.Error("Could not marshal PSSMsgShare struct")
+		return []byte("")
+	}
+	return byt
+}
+
+type PSSMsgSend struct {
+	PSSID  PSSID
+	C      [][]common.Point
+	A      []big.Int
+	Aprime []big.Int
+	B      []big.Int
+	Bprime []big.Int
+}
+
+type pssMsgSendBase struct {
+	PSSID  string
+	C      [][][2]string
+	A      []string
+	Aprime []string
+	B      []string
+	Bprime []string
+}
+
+type Parser interface {
+	MarshalI(big.Int) string
+	MarshalIA([]big.Int) []string
+	MarshalIM([][]big.Int) [][]string
+	UnmarshalI(string) big.Int
+	UnmarshalIA([]string) []big.Int
+	UnmarshalIM([][]string) [][]big.Int
+	MarshalP(common.Point) [2]string
+	MarshalPA([]common.Point) [][2]string
+	MarshalPM([][]common.Point) [][][2]string
+	UnmarshalP([2]string) common.Point
+	UnmarshalPA([][2]string) []common.Point
+	UnmarshalPM([][][2]string) [][]common.Point
+}
+
+var BaseParser = &parser{}
+
+type parser struct {
+}
+
+func (p *parser) MarshalI(arg big.Int) string {
+	return arg.Text(16)
+}
+func (p *parser) UnmarshalI(arg string) (res big.Int) {
+	res.SetString(arg, 16)
+	return
+}
+func (p *parser) MarshalIA(args []big.Int) (res []string) {
+	for _, i := range args {
+		res = append(res, p.MarshalI(i))
+	}
+	return
+}
+func (p *parser) UnmarshalIA(args []string) (res []big.Int) {
+	for _, i := range args {
+		res = append(res, p.UnmarshalI(i))
+	}
+	return
+}
+func (p *parser) MarshalIM(args [][]big.Int) (res [][]string) {
+	for _, i := range args {
+		res = append(res, p.MarshalIA(i))
+	}
+	return
+}
+func (p *parser) UnmarshalIM(args [][]string) (res [][]big.Int) {
+	for _, i := range args {
+		res = append(res, p.UnmarshalIA(i))
+	}
+	return
+}
+func (p *parser) MarshalP(arg common.Point) [2]string {
+	return [2]string{arg.X.Text(16), arg.Y.Text(16)}
+}
+func (p *parser) UnmarshalP(arg [2]string) common.Point {
+	var x, y big.Int
+	x.SetString(arg[0], 16)
+	y.SetString(arg[1], 16)
+	return common.Point{X: x, Y: y}
+}
+func (p *parser) MarshalPA(args []common.Point) (res [][2]string) {
+	for _, i := range args {
+		res = append(res, p.MarshalP(i))
+	}
+	return
+}
+func (p *parser) UnmarshalPA(args [][2]string) (res []common.Point) {
+	for _, i := range args {
+		res = append(res, p.UnmarshalP(i))
+	}
+	return
+}
+func (p *parser) MarshalPM(args [][]common.Point) (res [][][2]string) {
+	for _, i := range args {
+		res = append(res, p.MarshalPA(i))
+	}
+	return
+}
+func (p *parser) UnmarshalPM(args [][][2]string) (res [][]common.Point) {
+	for _, i := range args {
+		res = append(res, p.UnmarshalPA(i))
+	}
+	return
+}
+func (pssMsgSend *PSSMsgSend) ToBytes() []byte {
+	byt, _ := fastjson.Marshal(pssMsgSendBase{
+		string(pssMsgSend.PSSID),
+		BaseParser.MarshalPM(pssMsgSend.C),
+		BaseParser.MarshalIA(pssMsgSend.A),
+		BaseParser.MarshalIA(pssMsgSend.Aprime),
+		BaseParser.MarshalIA(pssMsgSend.B),
+		BaseParser.MarshalIA(pssMsgSend.Bprime),
+	})
+	return byt
+}
+
+func (pssMsgSend *PSSMsgSend) FromBytes(data []byte) error {
+	var p pssMsgSendBase
+	err := fastjson.Unmarshal(data, &p)
+	if err != nil {
+		return err
+	}
+	pssMsgSend.PSSID = PSSID(p.PSSID)
+	pssMsgSend.C = BaseParser.UnmarshalPM(p.C)
+	pssMsgSend.A = BaseParser.UnmarshalIA(p.A)
+	pssMsgSend.Aprime = BaseParser.UnmarshalIA(p.Aprime)
+	pssMsgSend.B = BaseParser.UnmarshalIA(p.B)
+	pssMsgSend.Bprime = BaseParser.UnmarshalIA(p.Bprime)
+	return nil
+}

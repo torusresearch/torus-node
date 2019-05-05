5128d3eab4e92070dbcb1be803f4086dbe4dd589
tetratorus <tetratorus@gmail.com>
2019-05-05 23:57:33 +1000

    Update test for solidity for whitelist

--- solidity/test/nodeList.js
+++ solidity/test/nodeList.js
@@ -1,13 +1,13 @@
-var NodeList = artifacts.require("NodeList.sol");
+var NodeList = artifacts.require("NodeList.sol")
 
 contract('Node List', async (accounts) => {
   beforeEach(async () => {
-    nodeList = await NodeList.new();
+    nodeList = await NodeList.new()
   })
 
-  it("should  list()", async () => {
-    var result = await nodeList.listNode("yoyo", 1000, 1000);
-    console.log(result.logs[0].args)
-    assert(result.logs[0].event === "NodeListed", "should emit event nodelist");
+  it("should whitelist", async () => {
+    await nodeList.updateWhitelist(1, "0x3ecefafea7db9d0e26dc0d266504587cb66f6008", true)
+    result = await nodeList.IsWhitelisted(1, "0x3ecefafea7db9d0e26dc0d266504587cb66f6008")
+    assert(result === true)
   })
 })

1efb4900ba46d4b93001fe03da22a0ae8f1dd3e8
Zhen Yu Yong <fuz3zy@gmail.com>
2019-05-06 11:41:44 +0800

    updated package json

--- solidity/truffle-config.js
+++ solidity/truffle-config.js
@@ -75,4 +75,19 @@ module.exports = {
     //   gasPrice: 10000000000, // 50 gwei, this is very high
     // },
   },
+    // Configure your compilers
+    compilers: {
+      solc: {
+        version: '0.5.7', // Fetch exact version from solc-bin (default: truffle's version)
+        // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+        settings: {
+          // See the solidity docs for advice about optimization and evmVersion
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+          // evmVersion: "byzantium"
+        }
+      }
+    }
 };
